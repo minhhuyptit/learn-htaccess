9e3c50008a40e6af3a9ae1ac0b1aa8b9022ba313
huy <minhhuy97.ptit@gmail.com>
2019-01-01 14:02:01 +0700

    Backup Data

---
+++ 08/result.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo '<h3>File result</h3>';
+        echo "<pre>";
+        print_r($_SERVER);
+        echo "</pre>";
+    ?>
+</body>
+</html>
